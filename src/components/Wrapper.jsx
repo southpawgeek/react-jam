b9b682bff39aef11b3a0a8ae0aeb203e433e4fd3
Jen Guerra <jen@southpawgeek.com>
2024-10-02 21:31:28 -0400

    increase size, add welcome

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,3 +1,27 @@
-const Wrapper = ({ children }) => <div id="wrapper">{children}</div>
+import { useState } from "react"
+
+const Wrapper = ({ children }) => {
+  const [welcome, showWelcome] = useState(true)
+
+  return (
+    <div id="wrapper">
+      {welcome ? (
+        <div id="welcome">
+          <p>
+            It's 1993 and you just finished installing a weird floppy disk on
+            your parents' i386. You're sure they won't mind. The label reads
+            "End Quest", and promises you "minutes of point and click fun!"
+          </p>
+          <br />
+          <hr />
+          <br />
+          <button onClick={() => showWelcome(false)}>[OK, whatever]</button>
+        </div>
+      ) : (
+        children
+      )}
+    </div>
+  )
+}
 
 export default Wrapper
